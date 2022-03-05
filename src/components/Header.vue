3d8aa90f11f373a6aab9256073a83e74acc50e45
Gotrunn <itsgotrunn@gmail.com>
2022-03-05 13:08:01 +0300

    1

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="header">
+		<p>Restaurant Menu (Header)</p>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+	width: 100vw;
+	height: 100px;
+	background-color: rgb(201, 201, 201);
+}
+</style>
